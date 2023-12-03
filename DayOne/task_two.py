1965af53723e238e5b443354512722499f3fb642
Finnegan1 <fynn.borrmeister@gmail.com>
2023-12-03 13:41:10 +0100

    .gitignore

--- DayOne/task_two.py
+++ DayOne/task_two.py
@@ -4,8 +4,7 @@ def find_all(a_str, sub):
         start = a_str.find(sub, start)
         if start == -1: return
         yield start
-        start += len(sub) # use start += 1 to find overlapping matches
-
+        start += len(sub) 
 
 numbers = [
     "one",
